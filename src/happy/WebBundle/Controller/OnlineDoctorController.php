7acc54975b822afd3254993e353cb0052c63abf5
gombo29 <gombo29@gmail.com>
2017-11-03 17:44:57 +0800

    emneleg tsahim emch zasav

--- src/happy/WebBundle/Controller/OnlineDoctorController.php
+++ src/happy/WebBundle/Controller/OnlineDoctorController.php
@@ -2,10 +2,8 @@
 
 namespace happy\WebBundle\Controller;
 
-use happy\CmsBundle\Entity\Content;
-use happy\CmsBundle\Entity\LaboratoryType;
-use happy\CmsBundle\Entity\Medicals;
-use happy\CmsBundle\Entity\MedicalType;
+use happy\CmsBundle\Entity\OnlineDoctorQuestion;
+use happy\CmsBundle\Entity\OnlineDoctorType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
@@ -13,14 +11,60 @@ use Symfony\Component\HttpFoundation\Request;
 
 class OnlineDoctorController extends Controller
 {
-
-
-
     /**
      * @Route("/doctor", name="online_doctor")
      */
     public function homeAction()
     {
         return $this->render('@happyWeb/OnlineDoctor/index.html.twig');
+    }
+
+    /**
+     * @Route("/doctor-question/{id}", name="online_doctor_question", requirements={"id" = "\d+"} )
+     */
+    public function questionAction($id)
+    {
+
+        $em = $this->getDoctrine()->getManager();
+        $qb = $em->getRepository('happyCmsBundle:OnlineDoctorType')->createQueryBuilder('n');
+        /**@var OnlineDoctorType[] $questions */
+        $questions = $qb
+            ->where('n.parent = :id')
+            ->setParameter('id', $id)
+            ->getQuery()
+            ->getArrayResult();
+
+        return $this->render('@happyWeb/OnlineDoctor/question.html.twig', array('questions' => $questions));
+    }
+
+    /**
+     * @Route("/doctor-answers/{id}", name="online_doctor_answers", requirements={"id" = "\d+"} )
+     */
+    public function answerAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $qb = $em->getRepository('happyCmsBundle:OnlineDoctorQuestion')->createQueryBuilder('n');
+
+        if ($request->get('isfirst') == 1) {
+            $qb->where('n.isFirst = 1')
+                ->andWhere('n.type = :id')
+                ->setParameter('id', $id);
+        } else {
+            $qb
+                ->where('n.id = :id')
+                ->setParameter('id', $id);
+        }
+
+
+        /**@var OnlineDoctorQuestion[] $answers */
+        $answers = $qb
+            ->leftJoin('n.childYes', 'yes')
+            ->leftJoin('n.childNo', 'no')
+            ->addSelect('yes')
+            ->addSelect('no')
+            ->getQuery()
+            ->getArrayResult();
+
+        return $this->render('@happyWeb/OnlineDoctor/answers.html.twig', array('answers' => $answers));
     }
 }
