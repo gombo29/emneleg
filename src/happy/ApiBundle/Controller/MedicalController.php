9e473957438d7f4377b7fd77cfd7bdb88b8b800e
gombo29 <gombo29@gmail.com>
2018-12-08 19:47:57 +0800

    Update MedicalController.php
    t

--- src/happy/ApiBundle/Controller/MedicalController.php
+++ src/happy/ApiBundle/Controller/MedicalController.php
@@ -149,7 +149,7 @@ class MedicalController extends Controller
      
           if ($labIds == null) {
             //  $labIds = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19];
-               $labIds = [0];
+               $labIds = [1];
           }
 
         if ($medicalMedIds != null) {
